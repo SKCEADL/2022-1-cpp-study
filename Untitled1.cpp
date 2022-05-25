bf84fde259ff6ecead5af3cca70f9b92212d5407
kangbomj <105841378+kangbomj@users.noreply.github.com>
2022-05-25 18:33:23 +0900

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float num1;
+	float num2;
+
+	cout << "첫번째 정수를 입력하시오.: ";
+	cin >> num1;
+	cout << "두번째 정수를 입력하시오 :";
+	cin >> num2;
+
+	cout << num1 << "/" << num2 << "=" << num1 / num2 << "\n";
+}
