191b0d4abfcdbe3e9a8ebf1c67210d702617278f
Heungkuki <105864403+Heungkuki@users.noreply.github.com>
2022-05-25 16:44:59 +0900

    Add files via upload

---
+++ 두 수 나누기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    float x;
+    float y;
+    cout << "첫 번째 수를 입력하세요.\n";
+    cin >> x;
+    cout << "두 번째 수를 입력하세요.\n";
+    cin >> y;
+    cout << "결과는" << x/y << "입니다.";
+
+    return 0;
+}
